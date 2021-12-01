c4499d770a2e112ea4cd1b4cb65e5646b896946a
seanfcb <sean.connolly.boutin@gmail.com>
2021-12-01 14:18:57 -0500

    Rolled back correction

--- CompressibleFlowFunctions/misc.py
+++ CompressibleFlowFunctions/misc.py
@@ -29,7 +29,7 @@ def flowrates_swagelok(T1,P2,P1,Cv,SG,Q):
     SG       : Specific gravity w.r.t. air
     Q        : Volumetric flow rate, SCFH (see mdot_to_scfh)
     '''
-    conv = 60*22.67*np.sqrt(9/5)#conversion constant
+    conv = 60*22.67*np.sqrt(5/9)#conversion constant
     delP = P1-P2
     return Q - conv*Cv*(1-(2/3)*delP/P1)*np.sqrt(delP/(P1*SG*T1))
 
