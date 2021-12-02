34d802cb814287c9686032b7d435a2ae30db6091
seanfcb <38043177+seanfcb@users.noreply.github.com>
2021-12-02 17:07:31 -0500

    Update misc.py

--- CompressibleFlowFunctions/misc.py
+++ CompressibleFlowFunctions/misc.py
@@ -83,7 +83,7 @@ def hole_numbers(Dhole,Astar):
 
 def spacer_sizing(t,Po,To,Rs,mdot,gamma,specs):
     from CompressibleFlowFunctions.Isentropic import area_from_mass
-    def delmass(delta_mass_stag(A,M,mdot,Po,Rs,To,gamma)):
+    def delmass(A,M,mdot,Po,Rs,To,gamma):
         return delta_mass_stag(M,mdot,Po,Rs,To,gamma,A)
 
     species = specs[0] #should be either fuel or oxidizer
