1d2e2606f6943a79baed358f0cb22df28dfe2fef
seanfcb <38043177+seanfcb@users.noreply.github.com>
2021-12-03 12:33:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=['numpy','scipy'],
 
     # *strongly* suggested for sharing
-    version='0.3',
+    version='0.5',
     # The license can be anything you like
     license='MIT',
     description='A package containing various functions in compressible flow',
